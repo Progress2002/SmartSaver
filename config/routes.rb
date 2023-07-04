bb6847caa9e79f7baed89a113a2cc31c48b3cc99
Progress2002 <ezeamakachisom2002@gmail.com>
2023-07-04 15:21:17 +0100

    Create and add styles to category show and add new entity page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # resources :categories
-  # resources :entities
-  # resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 
   get '/splash', to: 'splashes#index'
 
